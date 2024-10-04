293bc794ef87c7ec2dd3d6c3b6f5e22d98ac3ef9
Borja Zafra Aguilar <borja.zafra-aguilar.external@capgemini.com>
2024-10-04 10:54:20 +0200

    Ex3 as function

--- UD1/P1/Ejercicio3.php
+++ UD1/P1/Ejercicio3.php
@@ -1,14 +1,22 @@
 <?php
-$session_token = rand(1000, 10000);
-$continue = 's';
-while ($continue == 's') {
-    echo "Este es tu token de sesión: \n" . $session_token;
-    echo "\nTienes 5 segundoss para usarlo";
 
-    sleep(5);
-
-    $continue = readline("\n¿Quieres volver a solicitar un token? s/n : ");
-    ncurses_erase();
+function generateToken(){
+    $continue = true;
+    while ($continue) {
+        $session_token = rand(1000, 10000);
+    
+        echo "Este es tu token de sesión: \n" . $session_token;
+        echo "\nTienes 5 segundoss para usarlo";
+    
+        sleep(5);
+    
+        $option = readline("\n¿Quieres volver a solicitar un token? (s/n) : ");
+        if($option != "s")
+            $continue = false;
+            
+    }
+    echo "\nAdiós";
+    return;
 }
 
-echo "Adiós ";
+generateToken();
---
+++ UD1/P1/Ejercicio5.php
@@ -0,0 +1,4 @@
+<?php
+
+// Estandares RFC 5321 RFC 5322
+//
