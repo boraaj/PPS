d2a3c0783ea92f29ec59cb11ffb35212de54a63a
boraaj <borja_bza@hotmail.com>
2024-10-06 11:40:16 +0200

    first approach

--- UD1/P1/Ejercicio5.php
+++ UD1/P1/Ejercicio5.php
@@ -12,12 +12,12 @@
 
 
 
-function validacion_espacios_at_maxchars(string $email)
+function validacion_espacios_at_maxchars(string $email_entero)
 {
 
-    if (substr_count($email, "@") > 1 || substr_count($email, "@") < 1)
+    if (substr_count($email_entero, "@") > 1 || substr_count($email_entero, "@") < 1)
         return false;
-    if (str_contains($email, " ") || count_chars($email > 320))
+    if (str_contains($email_entero, " ") || count_chars($email_entero > 320))
         return false;
     return true;
 }
@@ -26,31 +26,60 @@ function validacion_local(string $parte_local): bool
 {
 
     $invalid_dots = "/\.{2,}/";
+    $inyection_chars_patters = "/['\" <>&;?!|%$()\[\]{}*+\-`~#]/";
+
+    //Contiene entre 1 y 64 caracteres
+    if (strlen($parte_local) > 64 || strlen($parte_local) < 1)
+        return false;
+    //contiene @ o punto al principio y al final. 
     if (substr_count($parte_local, "@") || $parte_local[0] == "." || substr($parte_local, -1) == ".")
         return false;
-
+    // posee mas de dos puntos consecutivos entre medio. 
     if (preg_match($invalid_dots, $parte_local))
         return false;
+    //Contiene algun caracter especial para hacer inyecciones. 
+    if (substr_count($parte_local, '"') == 2) {
+        $primera_comilla = strpos($parte_local, '"');
+        $segunda_comilla = strpos($parte_local, '"', $primera_comilla + 1);
+        $substring_comillas = substr($parte_local, $primera_comilla + 1, $segunda_comilla - $primera_comilla - 1);
+        if (preg_match($inyection_chars_patters, $substring_comillas))
+            return true;
+    } else
+        return false;
 
-
-
-
-
+    if (preg_match($inyection_chars_patters, $parte_local))
+        return false;
     return true;
+}
+
+function validacion_domain($parte_domain)
+{
+    $inyection_chars_patters = "/['\" <>&;?!|%$()\[\]{}*+\-`~#]/";
+    $invalid_dots = "/\.{2,}/";
+    if ($parte_domain[0] == "." || substr($parte_domain, -1) == ".")
+        return false;
+    if ($parte_domain[0] == "-" || substr($parte_domain, -1) == "-")
+        return false;
+    if (preg_match($invalid_dots, $parte_domain))
+        return false;
+    if (strlen($parte_domain) > 253)
+        return false;
+    if (preg_match($inyection_chars_patters, $parte_domain))
+        return false;
 }
 
 
 
-function email_valido(string $email): bool
+function validacion_mail(string $email_entero): bool
 {
 
-    if (validacion_espacios_at_maxchars($email))
+    if (validacion_espacios_at_maxchars($email_entero))
         return true;
 
     return false;
 }
 
 
-$input_email = readline("Insotruce tu correo electrónico: ");
+$input_email_entero = readline("Insotruce tu correo electrónico: ");
 
     // generales . 
